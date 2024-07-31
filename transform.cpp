36592e3e667c4f2ff68854600e6db9d6f5e4dd1f
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2024-07-31 12:17:37 +0930

    transform question

---
+++ transform.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int to_base2(int input) {
+    int array[32];
+    string str;
+    int current = input;
+
+    int index = 0;
+
+    while(current > 0) {
+        array[index] = current % 2;
+
+        current = current / 2;
+
+        index++;
+    } 
+
+    for (int i = index - 1; i>=0; i--) {
+        // cout << array[i] << endl;
+        str = str + to_string(array[i]);
+    }
+
+    int finalNum = stoi(str);
+
+    return finalNum;
+    
+}
+
+
+int main() {
+
+    
+    cout << to_base2(5) << endl;
+    return 0;
+}
