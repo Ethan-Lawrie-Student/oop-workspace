4e7f0d3448051ed69dc2dd333ee90dabfdbc4d41
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2024-08-01 22:19:42 +0930

    fixed

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
 
 void count_digits(int array[4][4]) {
+  int zeros, ones, twos, threes, fours, fives, sixes, sevens, eights, nines;
+  zeros = ones = twos = threes = fours = fives = sixes = sevens = eights =
+      nines = 0;
 
-    int zeros, ones, twos, threes, fours, fives, sixes, sevens, eights, nines;
-    zeros = ones = twos = threes = fours = fives = sixes = sevens = eights = nines = 0;
-
-    for (int i = 0; i<4; i++) {
-        for (int j = 0; j < 4; j++) {
-            switch (array[i][j]) {
-                case 1:
-                    zeros++;
-                    break;
-                case 2:
-                    twos++;
-                    break;
-                case 3:
-                    threes++;
-                    break;
-                case 4:
-                    fours++;
-                    break;
-                case 5:
-                    fives++;
-                    break;
-                case 6:
-                    sixes++;
-                    break;
-                case 7:
-                    sevens++;
-                    break;
-                case 8:
-                    eights++;
-                    break;
-                case 9:
-                    nines++;
-                    break;
-            }
-            
-        }
-        
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      switch (array[i][j]) {
+        case 0:
+          zeros++;
+          break;
+        case 1:
+          ones++;
+          break;
+        case 2:
+          twos++;
+          break;
+        case 3:
+          threes++;
+          break;
+        case 4:
+          fours++;
+          break;
+        case 5:
+          fives++;
+          break;
+        case 6:
+          sixes++;
+          break;
+        case 7:
+          sevens++;
+          break;
+        case 8:
+          eights++;
+          break;
+        case 9:
+          nines++;
+          break;
+      }
     }
+  }
 
-
-    std::cout << "0:" << zeros << ";1:" << ones <<";2:"  << twos << ";3:" << threes << ";4:" << fours << ";5:" << fives << ";6:" << sixes << ";7:" << sevens << ";8:" << eights << ";9:" << nines << ';' << std::endl;
+  std::cout << "0:" << zeros << ";1:" << ones << ";2:" << twos
+            << ";3:" << threes << ";4:" << fours << ";5:" << fives
+            << ";6:" << sixes << ";7:" << sevens << ";8:" << eights
+            << ";9:" << nines << ';' << std::endl;
 }
--- function-2-1.cpp
+++ function-2-1.cpp
@@ -4,33 +4,31 @@
 using namespace std;
 
 void print_binary_str(std::string decimal_number) {
+  int current = stoi(decimal_number);
+  // string str[9];
+  // int current = input;
+  int array[30];
 
-    int current = stoi(decimal_number);
-    // string str[9];
-    // int current = input;
-    int array[30];
+  int index = 0;
 
-    int index = 0;
+  while (current > 0) {
+    array[index] = current % 2;
 
-    while(current > 0) {
-        array[index] = current % 2;
-        cout << array[index];
+    current = current / 2;
 
-        current = current / 2;
+    index++;
+  }
+  for (int i = index - 1; i >= 0; i--) {
+    cout << array[i];
+  }
 
-        index++;
-    } 
+  cout << "\n";
 
-    cout<< "\n";
+  // int outputNum = 0;
 
-    
+  // for (int i = 0; i< index; i++) {
+  //     outputNum = outputNum * 10 + array[i];
+  // }
 
-    // int outputNum = 0;
-
-    // for (int i = 0; i< index; i++) {
-    //     outputNum = outputNum * 10 + array[i];
-    // }
-
-    // cout << "\n" << outputNum << endl;
-
+  // cout << "\n" << outputNum << endl;
 }
