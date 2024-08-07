1a0e4914aa10da344f02b80058e633fd07959b6c
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2024-08-07 12:51:03 +0930

    fix

--- workshop.h
+++ workshop.h
@@ -26,7 +26,7 @@ void printArray(double* array, int size) {
 }
 
 double arrayMax(double* array, int size) {
-    int max = array[0];
+    double max = array[0];
 
     for(int i = 1; i < size; i++) {
         if (array[i] > max) {
