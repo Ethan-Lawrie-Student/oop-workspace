4ef1974f2dc58a8a7ed9a90dcb3ae7b73ef203b6
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2024-08-07 12:47:17 +0930

    finished questions

--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
 #include "workshop.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // part1();
+    // double x = 31;
+    // cout << x << endl;
+    // changeValue(&x);
+    // cout << x << endl;
+
+    // double array[4] = {1,4,12,3};
+
+    // cout << arrayMax(array, 4) << endl;
+
+    double *reference = dynamicArray(5, 22);
+
+    // cout << arrayMax(reference, 5) << endl;
+
+    // delete[] reference;
+
+    cout << "\n";
+
+    for (int i = 0; i < 5; i++) {
+        
+        cout << reference[i] << endl;
+    }
+
+
+
+    return 0;
+}
---
+++ workshop.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <iostream>
+
+void part1() {
+    double value = 10;
+    char character = 'a';
+    double *valueRef;
+    char *charRef;
+
+    valueRef = &value;
+    charRef = &character;
+
+    // std::cout << value << ", " << valueRef << std::endl;
+    printf("value: %f, valueRef: %p\n",value, valueRef);
+    printf("char: %c, charRef: %p\n",character, charRef);
+}
+
+void changeValue(double* doubleVal) {
+    *doubleVal = 42; 
+}
+
+void printArray(double* array, int size) {
+    for(int i = 0; i < size; i++) {
+        std::cout << array[i] << std::endl;
+    }
+}
+
+double arrayMax(double* array, int size) {
+    int max = array[0];
+
+    for(int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
+}
+
+double* dynamicArray(int N, double M) {
+    double *array = new double[N];
+    for (int i = 0; i<N; i++) {
+        
+        array[i] = M;
+        std::cout << array[i] << std::endl;
+    } 
+
+    return array;  
+}
