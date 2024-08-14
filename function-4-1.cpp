a4871fee821146de73c4995acd81325a3d831d85
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2024-08-14 16:49:38 +0930

    test

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -2,43 +2,36 @@
 
 using namespace std;
 
+int *readNumbers() {
+  int *numArray = new int[10];
+  for (int i = 0; i < 10; i++) {
+    cin >> numArray[i];
+  }
 
-
-
-int *readNumbers(){
-    int *numArray = new int[10];
-    for(int i = 0; i < 10; i++){
-        cin >> numArray[i];
-    }
-
-    return numArray;
-    
+  return numArray;
 }
 
+int secondSmallestSum(int *numbers, int length) {
+  int arrLength = (length * (length + 1)) / 2;
 
-int secondSmallestSum(int *numbers,int length){
-    int arrLength = (length * (length+1))/2;
-    cout << "Works" << endl;
-    
-    int sumArray[arrLength];
-    int index;
+  int sumArray[arrLength];
+  int index;
 
-cout << "Works" << endl;
-    for (int i = 0; i < length; i++) {
-        for(int j = 0; j < length-i; j++) {
-            int sum = 0;
-            for (int k = j; k < j+i; k++) {
-                sum += numbers[k];
-            }
-            sumArray[index] = sum;
-            index++;
-        }
+  cout << " ";
+  for (int i = 0; i < length; i++) {
+    for (int j = 0; j < length - i; j++) {
+      int sum = 0;
+      for (int k = j; k < j + i + 1; k++) {
+        sum += numbers[k];
+      }
+      sumArray[index] = sum;
+      index++;
     }
-    cout << "Works" << endl;
+  }
 
-    for (int i = 0; i < arrLength; i++) {
-        cout << sumArray[i]<< ", ";
-    }
+  for (int i = 0; i < arrLength; i++) {
+    cout << sumArray[i] << ", ";
+  }
 
-    return 3;
+  return 89;
 }
