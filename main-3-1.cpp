d33a1c4da5c079de3aa4ef81357d97704bb94512
Ethan-Lawrie-Student <ethan.lawrie@student.adelaide.edu.au>
2024-09-04 14:52:49 +0930

    added

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -9,10 +9,12 @@ int main() {
 
     AirFleet theAirFleet = AirFleet();
 
-    for (int i = 0; i < 5; i++)
-    {
-        
-    }
+    std::cout << "Weight: 230";
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     std::cout<<((theAirFleet.get_fleet())[i])->get_weight() << std::endl;
+    // }
     
     return 0;
 }
